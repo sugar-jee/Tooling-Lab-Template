4378206c20672aa30fa202a80ae66b576e0845fe
sugar-jee <stevengm@mail.uc.edu>
2025-03-30 17:14:10 -0400

    set up tests

---
+++ tests/3.rock_paper_scissors-logic.test.js
@@ -0,0 +1,36 @@
+const { RockPaperScissors } = require(`../resources/scripts/rock_paper_scissors.js`);
+
+describe(`RockPaperScissors class`, function () {
+  describe(`determineWinner()`, function () {
+    test(`win cases`, function () {
+      const game = new RockPaperScissors();
+      expect(game.determineWinner(`rock`, `scissors`)).toBe(`win`);
+      // Complete the test
+    });
+
+    test(`tie cases`, function () {
+      // Write your test here
+    });
+
+    test(`lost cases`, function () {
+      // Write your test here
+    });
+  });
+});
+
+describe(`generateCPUResponse()`, function () {
+  it(`Math.Random = 0.1 -> Rock`, function() {
+    mathRandomSpy.mockImplementation(() => 0.1);
+    const game = new RockPaperScissors();
+    expect(game.generateCPUResponse()).toBe(`rock`);
+  });
+
+  it(`Math.Random = 0.5 -> Paper`, function() {
+    // Write your test here
+  });
+
+  it(`Math.Random = 0.9 -> Paper`, function() {
+    // Write your test here
+  });
+
+});
